3bf38bf701954946efd1cb043ed51fa13db0355a
iglesias.kaiet@opendeusto.es <iglesias.kaiet@opendeusto.es>
2022-04-28 16:18:34 +0200

    Subiendo los datos

--- desgranaje_cloud_role.py
+++ desgranaje_cloud_role.py
@@ -28,6 +28,7 @@ body = get_policy_body(POLICY_ARN)
 
 #print(body['Statement'][0]['Condition'])
 
+body = {'requestContext': {'elb': {'targetGroupArn': 'arn:aws:elasticloadbalancing:us-east-1:070307590085:targetgroup/tg-lambda/20de418ff8a7c880'}}, 'httpMethod': 'GET', 'path': '/', 'queryStringParameters': {}, 'headers': {'accept-encoding': 'gzip', 'host': 'example.com', 'user-agent': 'Go-http-client/1.1', 'x-amzn-trace-id': 'Root=1-626a7da4-252f362c3a2df46c476c7af4', 'x-forwarded-for': '45.148.10.81', 'x-forwarded-port': '80', 'x-forwarded-proto': 'http'}, 'body': '', 'isBase64Encoded': False}
 
 
 
@@ -36,25 +37,29 @@ for element in body.keys():
     print('\n ', 'Elemento: ', element, ' :: \n ')
 
     
-    if isinstance(body[element][0], dict):
+    if isinstance(body[element], dict):
 
-       for k, v in body[element][0].items():
+       for k, v in body[element].items():
            
            print('\t KEY PRINCIPAL: ', k,' || value:  ',v, ' ')
 
-           if isinstance(body[element][0][k], dict): 
+           if isinstance(body[element][k], dict): 
 
-               for subkey, subvalue in body[element][0][k].items():
+               for subkey, subvalue in body[element][k].items():
 
                    print(' \n', '\t \t SUB-KEY PRINCIPAL: ', subkey, ' ', subvalue)
 
-                   if isinstance(body[element][0][k][subkey], dict):
+                   if isinstance(body[element][k][subkey], dict):
 
-                       for susubkey, subsubvalue in body[element][0][k][subkey].items():
+                       for susubkey, subsubvalue in body[element][k][subkey].items():
 
                            print(' \n', '\t \t \t SUBSUB-KEY PRINCIPAL: ', susubkey, ' ', subsubvalue)
 
                #print('DIC')
+
+    else: 
+
+        print('\t \t Su elemento: ', body[element])
 
            
 
